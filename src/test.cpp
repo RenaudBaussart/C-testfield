2d302c8ab2d84f1ccc5be4b761610e42aac6be0c
renaudbaussart <renaud.baussart@proton.me>
2024-04-27 18:56:58 +0200

    build: first try of a c++ programe

---
+++ src/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+    int EndNbr = 2;
+    if (EndNbr == 10)
+    {
+        std::cout << "congrad good loop";
+
+    }
+    else
+    {
+        EndNbr = EndNbr + 1;
+    }
+}
