9e9eb81b6727a00a9bf0567a25d784a520a0574f
renaudbaussart <renaud.baussart@proton.me>
2024-05-01 19:52:09 +0200

    build(test.cpp): tried loop first time

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 int main()
 {
+  int nbr1;
+  nbr1 = 4;
   std::cout << "hello world";
   return 0;
+  while (nbr1 <= 8)
+  {
+    cout << nbr1;
+    nbr1 = nbr1 + 1;
+  }
+  
 }
