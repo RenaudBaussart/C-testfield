aa65b2d2069b188b0fcf7a5ca254c909e314329a
renaudbaussart <renaud.baussart@proton.me>
2024-05-06 11:45:41 +0200

    feat(while-loop.cpp): create a exemple of a while loop

---
+++ src/while-loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+  int nbr1 = 1;
+  while (nbr1 < 8){
+    cout << nbr1;
+    nbr1 ++;
+  }
+  return 0;
+}
