61c11ece2af29f7508c23599efa412907759745e
renaudbaussart <renaud.baussart@proton.me>
2024-05-06 14:41:04 +0200

    feat(for-loop.cpp): create exemple of for statement

---
+++ src/Cpp-statement-exemple/for-loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main(){
+  for (int i= 0; i < 10;i++){
+    cout << "detonation in : " << i << endl;
+  }
+  return 0;
+}
