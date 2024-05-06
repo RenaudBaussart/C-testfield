b5319b22e8ddaead1149ddcdc12d4180143f2d10
renaudbaussart <renaud.baussart@proton.me>
2024-05-06 11:47:12 +0200

    test(test.cpp): reset file

--- src/test.cpp
+++ src/test.cpp
@@ -3,16 +3,5 @@
 #include <string>
 
 using namespace std;
-int main()
-{
-  int nbr1;
-  nbr1 = 4;
-  std::cout << "hello world";
-  return 0;
-  while (nbr1 <= 8)
-  {
-    cout << nbr1;
-    nbr1 = nbr1 + 1;
-  }
-  
+int main(){
 }
