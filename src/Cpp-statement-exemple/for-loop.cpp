7a847c39474f8208ac8f85f81ce120360a9180de
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-08 22:40:42 +0200

    feat(for-loop.cpp): add hint in the code

--- src/Cpp-statement-exemple/for-loop.cpp
+++ src/Cpp-statement-exemple/for-loop.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 int main(){
+//start of the loop here util i is under 10
   for (int i= 0; i < 10;i++){
+    //print in the consol a comment followed by the number in
     cout << "detonation in : " << i << endl;
   }
   return 0;
