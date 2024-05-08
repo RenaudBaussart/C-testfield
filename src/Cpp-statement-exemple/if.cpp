cf4dcef66eab188ffeffb8655730ded558335e3b
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-08 22:51:17 +0200

    feat(if.cpp): add hint in the code

--- src/Cpp-statement-exemple/if.cpp
+++ src/Cpp-statement-exemple/if.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 int main(){
-  int nbrimput;
+  int nbrimnut;//declaration of the varable nbrinput
   cout << "entre un nombre entier\n";
-  cin >> nbrimput;
-  if (nbrimput < 500){
+  cin >> nbrinput;//get the input from the terminal into nbrimpute
+  if (nbrimput < 500){//if under 500 do this
     cout << "tu a entrée un nombre en dessous de 500\n";
   }
-  if (nbrimput > 500){
+  if (nbrimput > 500){//if highter 500 do this
     cout << "tu a entrée un nombre aux desus de 500\n";
   }
-  else{
+  else{//for all other instance do that
     cout << "tu a entrée 500\n";
   }
   return 0;
