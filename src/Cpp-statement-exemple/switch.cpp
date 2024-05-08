d0a0fdde50ae334bbf2cb8074d53a81dd2637676
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-09 00:48:01 +0200

    feat(switch.cpp): add hint to code

--- src/Cpp-statement-exemple/switch.cpp
+++ src/Cpp-statement-exemple/switch.cpp
@@ -9,22 +9,22 @@ int main(){
   cin >> nbr;
   switch (nbr)
   {
-  case 1:
+  case 1://in case the nbr var is 1 do that
     cout << "well play you have enter:" << nbr << endl;
     break;
-  case 2:
+  case 2://in case the nbr var is 2 do that
     cout << "well play you have enter:" << nbr << endl;
     break;
-  case 3:
+  case 3://in case the nbr var is 3 do that
     cout << "well play you have enter:" << nbr << endl;
     break;
-  case 4:
+  case 4://in case the nbr var is 4 do that
     cout << "well play you have enter:" << nbr << endl;
     break;
-  case 5:
+  case 5://in case the nbr var is 5 do that
     cout << "well play you have enter:" << nbr << endl;
     break;
-  default:
+  default://in other case 
     cout << "plz enter a number between 1 and 5\n";
     break;
   }
