8dafcbd85afd1652a3487ac06509c1dd76927784
renaudbaussart <renaud.baussart@proton.me>
2024-05-09 21:35:28 +0200

    feat(while-loop.cpp): add hit to the code

--- src/Cpp-statement-exemple/while-loop.cpp
+++ src/Cpp-statement-exemple/while-loop.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 int main()
 {
-  int nbr1 = 1;
-  while (nbr1 < 8){
+  int nbr1 = 1;//set-up of the variable nbr1
+  while (nbr1 < 8){//while nbr1 lower than 8 do this
     cout << nbr1;
-    nbr1 ++;
+    nbr1 ++;//add 1 to nbr1 var
   }
   return 0;
 }
